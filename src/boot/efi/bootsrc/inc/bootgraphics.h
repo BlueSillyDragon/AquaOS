5b4d47b5b5dc82ac56bedab51aeee67109fb574f
BlueSillyDragon <137140267+BlueSillyDragon@users.noreply.github.com>
2024-01-06 14:51:42 -0700

    Delete src/boot/efi/bootsrc/inc directory

--- src/boot/efi/bootsrc/inc/bootgraphics.h
+++
@@ -1,8 +0,0 @@
-#ifndef BOOTGRAPHICS_H
-#define BOOTGRAPHICS_H
-
-void displayLogo(int x, int y, int offX, int offY, uint32_t pixel, EFI_GRAPHICS_OUTPUT_PROTOCOL* gop);
-
-void plotPixels (int x, int y, uint32_t pixel, EFI_GRAPHICS_OUTPUT_PROTOCOL* gop);
-
-#endif

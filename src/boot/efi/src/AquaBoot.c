674879e4148d6cba3189d67eef4c2ff2c355d377
BlueSillyDragon <BlueSillyDragon@outlook.com>
2024-10-01 14:15:32 -0600

    Basic print() implmented and bpanic()

--- src/boot/efi/src/AquaBoot.c
+++ src/boot/efi/src/AquaBoot.c
@@ -1,27 +1,103 @@
 #include <efi.h>
+#include <stdarg.h>
 #include "inc/video_services.h"
 #include "inc/logo.h"
 
 #define AQUABOOT_MAJOR 0
 #define AQUABOOT_MINOR 1
 #define AQUABOOT_PATCH 0
-#define AQUABOOT_BUILD 1
 
 #define AQUABOOT_BG 0x2B60DE
 
+EFI_SYSTEM_TABLE *sysT = NULL;
+EFI_HANDLE imgH = NULL;
+
 void hlt() {
     asm volatile ( "hlt" );
+}
+
+void bpanic(void) {
+
+    sysT->ConOut->OutputString(sysT->ConOut, L"BOOTLOADER PANIC! ABORTING...");
+    sysT->BootServices->Stall(10 * 1000 * 1000);
+    
+    sysT->BootServices->Exit(imgH, EFI_ABORTED, 0, NULL);
+}
+
+void bdebug(EFI_SERIAL_IO_PROTOCOL* ser, char* str) {
+    UINTN strBuf = sizeof(str);
+    ser->Write(ser, &strBuf, (void *)str);
+}
+
+void print(CHAR16 *fmt, ...) {
+    CHAR16 c[2];
+    int int_to_print;
+    int number[100];
+    int j;
+    va_list args;
+
+    c[0] = u'\0', c[1] = u'\0';
+
+    va_start(args, fmt);
+
+    for (int i = 0; fmt[i] != u'\0'; i++) {
+        if (fmt[i] == u'%') {
+            i++; // Get character after %
+
+            switch (fmt[i]) {
+                case 'c':
+                    c[0] = va_arg(args, int);
+                    sysT->ConOut->OutputString(sysT->ConOut, c);
+                    break;
+                case 'd':
+                    int_to_print = va_arg(args, int);
+                    j = 0;
+                    do {
+                        number[j] = (int_to_print % 10);
+                        int_to_print = (int_to_print - int_to_print % 10) / 10;
+                        j++;
+                    }
+                    while(int_to_print > 0);
+
+                    j--;
+
+                    for (; j>=0; j--) {
+                        c[0] = number[j] + '0';
+                        sysT->ConOut->OutputString(sysT->ConOut, c);
+                    }
+
+                    continue;
+                default:
+                    break;
+            }
+        }
+
+        else {
+            c[0] = fmt[i];
+            sysT->ConOut->OutputString(sysT->ConOut, c);
+        }
+    }
+
+    va_end(args);
+
 }
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 	(void)ImageHandle;
     EFI_STATUS status;
 
+    sysT = SystemTable;
+    imgH = ImageHandle;
+
     // Clear the screen
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
 
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"AquaBoot loaded!\r\n");
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Detecting Block and Disk I/O Protocols...\r\n");
+    print(u"AquaBoot loaded!\r\n");
+    print(u"AquaBoot version %d.%d.%d\r\n",
+                                            AQUABOOT_MAJOR,
+                                            AQUABOOT_MINOR,
+                                            AQUABOOT_PATCH);
+    print(u"Detecting Serial I/O Protocol...\r\n");
 
     //AQUABOOT_FRAMEBUFFER framebuffer;
 
@@ -47,7 +123,29 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     //    }
     //}
 
+    // Setup Serial I/O
+    EFI_GUID serialGuid = EFI_SERIAL_IO_PROTOCOL_GUID;
+    EFI_SERIAL_IO_PROTOCOL *serial;
+
+    status = SystemTable->BootServices->LocateProtocol(&serialGuid, NULL, (void**)&serial);
+
+    if (EFI_ERROR(status)) {
+        print(u"Unable to locate Serial I/O Protocol!\r\n");
+    }
+
+    else {
+        print(u"Serial I/O Protocol located!\r\n");
+        UINTN bufferSize;
+        char buffer[] = "[Bootloader] \033[34mThis is some blue text!\033[0m\r\n";
+        bufferSize = sizeof(buffer);
+
+        serial->Reset(serial);
+        serial->Write(serial, &bufferSize, (void *)buffer);
+    }
+
     // Detect Disks
+
+    print(u"Detecting Block and Disk I/O Protocols...\r\n");
     
     EFI_GUID blockIoGuid = EFI_BLOCK_IO_PROTOCOL_GUID;
     EFI_BLOCK_IO_PROTOCOL *blockIo;
@@ -57,23 +155,23 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     status = SystemTable->BootServices->LocateProtocol(&blockIoGuid, NULL, (void**)&blockIo);
 
     if (EFI_ERROR(status)) {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Unable to locate Block I/O Protocol!\n\r");
+        print(u"Unable to locate Block I/O Protocol!\r\n");
         hlt();
     }
 
     else {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Block I/O Protocol located!\n\r");
+        print(u"Block I/O Protocol located!\r\n");
     }
 
     status = SystemTable->BootServices->LocateProtocol(&diskIoGuid, NULL, (void**)&diskIo);
 
     if (EFI_ERROR(status)) {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Unable to locate Disk I/O Protocol!\n\r");
+        print(u"Unable to locate Disk I/O Protocol!\r\n");
         hlt();
     }
 
     else {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Disk I/O Protocol located!\n\r");
+        print(u"Disk I/O Protocol located!\r\n");
     }
 
     BOOLEAN extendedVerification = 0;
@@ -81,7 +179,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     status = blockIo->Reset(blockIo, extendedVerification);
 
     if (EFI_ERROR(status)) {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"ERROR RESETTING BLOCK DEVICE!\n\r");
+        print(u"ERROR RESETTING BLOCK DEVICE!\r\n");
     }
 
     for(;;);
